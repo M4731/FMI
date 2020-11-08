be6ab07d913d8bcdf8eaf196b364ed62e099689b
mateieboss <mateimindruleanu@gmail.com>
2020-11-08 20:46:25 +0200

    WAD lab4

---
+++ Year II/Web Applications Development/lab_4/laboratorul_4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="laboratorul_4.MvcApplication" Language="C#" %>
---
+++ Year II/Web Applications Development/lab_4/laboratorul_4/Models/Article.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace laboratorul_4.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        //cheia externa
+        public int CategoryId { get; set; }
+
+        //unde refera cheia externa
+        public virtual Category Category { get; set; }
+    }
+
+    public class ArticleDBContext : DbContext
+    {
+        public ArticleDBContext() : base("DBConnectionString") { }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
+
+
